d4666244ffcafcdfbb74c3e020ff940572748d15
steven krawczyk <stevent.krawczyk@gmail.com>
2022-07-23 15:58:03 -0700

    Add batch upload API

--- app/ingestion_server.py
+++ app/ingestion_server.py
@@ -3,11 +3,14 @@ from flask import jsonify
 from flask import request
 from flask_cors import CORS
 from flask_restful import Resource, Api
+import pandas as pd
 
 from app.main_datastore.main_datastore_proxy import MainDatastoreProxy
+from app.main_datastore.dataframe_ingestion_client import DataframeIngestionClient
 
 
 database = MainDatastoreProxy()
+client = DataframeIngestionClient(database)
 
 app = Flask(__name__)
 CORS(app)
@@ -30,7 +33,21 @@ class Upload(Resource):
                         "status": 200})
 
 
+class Batch(Resource):
+    def post(self):
+        filepath = ""
+        for key, arg in request.args.items():
+            if key == "filepath":
+                filepath = arg
+        data = pd.read_csv(filepath, header=0)
+        client.upload(data)
+        return jsonify({"message": "",
+                        "category": "success",
+                        "status": 200})
+
+
 api.add_resource(Upload, '/upload')
+api.add_resource(Batch, '/batch')
 
 if __name__ == '__main__':
     app.run(debug=True)

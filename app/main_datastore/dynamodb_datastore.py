b1e9a10ba69cfe4959160914f6dcafd268cc956a
steven krawczyk <stevent.krawczyk@gmail.com>
2022-07-23 13:07:15 -0700

    Cleanup comments

--- app/main_datastore/dynamodb_datastore.py
+++ app/main_datastore/dynamodb_datastore.py
@@ -1,14 +1,3 @@
-'''
-This is the client for the primary storage for raw input to the system,
-ingested from web scrapers, manual uploads, and user input.
-
-NOTE: The current implementation uses a simple in memory database, a
-dictionary. At scale, we will need to replace this with a big data
-key-value store that supports appending to list values.
-
-TODO: We need our schema to protect against duplicate reviews. Today
-it does not.
-'''
 from decimal import localcontext
 import boto3
 from boto3.dynamodb.conditions import Key

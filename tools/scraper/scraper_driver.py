36cde4140b53476bb239d9569903f191a5cbc099
steven krawczyk <stevent.krawczyk@gmail.com>
2022-07-23 21:38:10 -0700

    Add metrics helper to driver

--- tools/scraper/scraper_driver.py
+++ tools/scraper/scraper_driver.py
@@ -2,6 +2,7 @@
 This will be the module used to drive the scraper process.
 '''
 from tools.scraper.data_submission_client import DataSubmissionClient
+from tools.scraper.scraper_metrics_helper import ScraperMetricsHelper
 from tools.scraper.web_scraper import WebScraper
 from tools.scraper.web_scraper_engine import WebScraperEngine
 
@@ -10,6 +11,7 @@ def main():
     data_submission_client = DataSubmissionClient()
     web_scraper_engine = WebScraperEngine()
     web_scraper = WebScraper(data_submission_client, web_scraper_engine)
+    metrics_helper = ScraperMetricsHelper()
 
     # TODO Logic to kick off web scraper jobs, track and store results
     # Things to track and store:

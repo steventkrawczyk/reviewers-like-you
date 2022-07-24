d2d118aa12c096a452b31c0f29891abaa892ce7a
steven krawczyk <stevent.krawczyk@gmail.com>
2022-07-23 23:04:52 -0700

    Update submission client

--- tools/scraper/data_submission_client.py
+++ tools/scraper/data_submission_client.py
@@ -4,6 +4,7 @@ data store, either directly or by saving the data off to a CSV for
 upload at a later time.
 '''
 import os
+from typing import List, Tuple
 from pandas import DataFrame
 
 from app.ingestion.main_datastore_proxy import MainDatastoreProxy
@@ -11,18 +12,21 @@ from app.ingestion.dataframe_ingestion_client import DataframeIngestionClient
 
 
 class DataSubmissionClient:
-    def __init__(self, filepath: str = ""):
+    def __init__(self, filepath: str = "data/scraped_data.csv"):
         if filepath != "":
             self.filepath = filepath
         else:
             self.database = MainDatastoreProxy()
             self.client = DataframeIngestionClient(self.database)
 
-    def submit(self, data: DataFrame) -> None:
+    def submit_dataframe(self, data: DataFrame) -> None:
         if self.filepath != "":
             data.to_csv(self.filepath, mode='a+')
         else:
             self.client.upload(data)
+
+    def submit_batch(self, reviews: List[Tuple[str, str, str]]) -> None:
+        self.database.batch_upload(reviews)
 
     def submit(self, author: str, movie: str, rating: str) -> None:
         if self.filepath != "":

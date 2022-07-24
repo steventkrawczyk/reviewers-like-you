ccd67fc566b8d42d5e4571458029c70fd8ffb575
steven krawczyk <stevent.krawczyk@gmail.com>
2022-07-23 22:22:02 -0700

    Add driver logic

--- tools/scraper/scraper_driver.py
+++ tools/scraper/scraper_driver.py
@@ -1,6 +1,7 @@
 '''
 This will be the module used to drive the scraper process.
 '''
+import sys
 from tools.scraper.data_submission_client import DataSubmissionClient
 from tools.scraper.scraper_metrics_helper import ScraperMetricsHelper
 from tools.scraper.web_scraper import WebScraper
@@ -9,15 +10,26 @@ from tools.scraper.web_scraper_engine import WebScraperEngine
 BATCH_SIZE = 25
 
 def main():
+    print("Initializing...")
     data_submission_client = DataSubmissionClient()
     web_scraper_engine = WebScraperEngine()
     web_scraper = WebScraper(data_submission_client, web_scraper_engine)
     metrics_helper = ScraperMetricsHelper()
 
-    metrics_helper.start_timer()
-    scraped = web_scraper.scrape_many_entires(BATCH_SIZE)
-    metrics_helper.end_timer()
-    metrics_helper.emit_metric(scraped, BATCH_SIZE)
+    total_entries = int(sys.argv[1:][0])
+    print("Attempting to scrape " + str(total_entries) + " entries overall.")
+
+    while total_entries > 0:
+        entries_to_scrape = min(BATCH_SIZE, total_entries)
+        print("Attempting to scrape a batch of " + str(entries_to_scrape) + " entries...")
+        metrics_helper.start_timer()
+        scraped = web_scraper.scrape_many_entires(entries_to_scrape)
+        metrics_helper.end_timer()
+        metrics_helper.emit_metric(scraped, entries_to_scrape)
+        print("Scraped " + str(scraped) + " entries!")
+        total_entries -= scraped
+    
+    print("Done scraping.")
 
 
 if __name__ == "__main__":

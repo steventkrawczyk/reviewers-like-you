67be7d6173db8bb5a510d107a59a7df5badb2f37
steven krawczyk <stevent.krawczyk@gmail.com>
2022-07-24 12:49:30 -0700

    test cleanup

--- tests/in_memory_tests.py
+++ tests/in_memory_tests.py
@@ -1,43 +1,47 @@
+from app.ingestion.main_datastore_proxy import MainDatastoreProxy
+from app.ingestion.dataframe_ingestion_client import DataframeIngestionClient
+from app.projection.projection_datastore_factory import ProjectionDatastoreFactory
+from app.projection.projection_engine import ProjectionEngine
+from app.recommendation.match_generator_factory import MatchGeneratorFactory
 import pandas as pd
 from pathlib import Path
 import sys
 import unittest
 
 sys.path.append("..")
-from app.recommendation.match_generator_factory import MatchGeneratorFactory
-from app.projection.projection_engine import ProjectionEngine
-from app.projection.projection_datastore_factory import ProjectionDatastoreFactory
-from app.ingestion.dataframe_ingestion_client import DataframeIngestionClient
-from app.ingestion.main_datastore_proxy import MainDatastoreProxy
 
 TEST_DATA_FILE = Path(__file__).parent / "test_data.csv"
 
 
-class ProjectionTests(unittest.TestCase):
+class InMemoryTests(unittest.TestCase):
     def setUp(self):
-        # Upload data
         self.test_data = pd.read_csv(TEST_DATA_FILE, header=0)
         self.database = MainDatastoreProxy(in_memory=True)
+        self.projection_databse = ProjectionDatastoreFactory(
+            in_memory=True).build()
+
+    def _do_ingestion(self):
         self.client = DataframeIngestionClient(self.database)
         self.client.upload(self.test_data)
 
-        # Create projection
-        self.authors = list(self.database.get_keys())
-        self.projection_databse = ProjectionDatastoreFactory(in_memory=True).build()
+    def _do_projection(self):
         self.projection_engine = ProjectionEngine(
             self.database, self.projection_databse)
         self.projection_engine.create_projection()
 
-        # Create match generator
+    def _do_recommendation(self):
         self.match_generator = \
-            MatchGeneratorFactory(self.database, 
+            MatchGeneratorFactory(self.database,
                                   self.projection_databse).build()
-
-    def test_get_match(self):
         test_user_input = {'bladerunner': 0.4}
         match = self.match_generator.get_match(test_user_input)
         self.assertEqual(match[0], 'steven',
                          'wrong match')
+    
+    def test_pipeline(self):
+        self._do_ingestion()
+        self._do_projection()
+        self._do_recommendation()
 
 
 if __name__ == '__main__':

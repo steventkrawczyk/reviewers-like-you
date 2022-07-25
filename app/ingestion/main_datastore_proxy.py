72d45970462db63d60cdbbe9a686f5b65a39cdec
steven krawczyk <stevent.krawczyk@gmail.com>
2022-07-24 20:52:35 -0700

    Use ABC

--- app/ingestion/main_datastore_proxy.py
+++ app/ingestion/main_datastore_proxy.py
@@ -2,13 +2,13 @@
 This is the client for the primary storage for raw input to the system,
 ingested from web scrapers, manual uploads, and user input.
 '''
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 from typing import List, Set
 
 from app.model.review import Review
 
 
-class MainDatastoreProxy:
+class MainDatastoreProxy(ABC):
 
     @abstractmethod
     def upload(self, review: Review) -> None:

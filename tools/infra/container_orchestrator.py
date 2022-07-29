b6b866b40d37ff213a2038183e178cfa1d724921
steven krawczyk <stevent.krawczyk@gmail.com>
2022-07-28 18:07:39 -0700

    Delay before create table

--- tools/infra/container_orchestrator.py
+++ tools/infra/container_orchestrator.py
@@ -1,4 +1,5 @@
 import subprocess
+import time
 
 
 class ContainerOrchestrator:
@@ -8,6 +9,7 @@ class ContainerOrchestrator:
     def start_containers(self):
         docker_compose_up = subprocess.Popen("docker compose up -d", shell=True)
         docker_compose_up.wait()
+        time.sleep(5)
 
     def stop_containers(self):
         docker_compose_down = subprocess.Popen("docker compose down", shell=True)

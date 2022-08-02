b4181b4827354d3975b515dea9ae317a4490c271
steven krawczyk <stevent.krawczyk@gmail.com>
2022-08-02 16:02:38 -0700

    Wait for services to up

--- scripts/start_local_website.sh
+++ scripts/start_local_website.sh
@@ -3,29 +3,18 @@
 docker compose up dynamodb-local -d
 docker compose up minio -d
 echo "Waiting for databases to startup..."
-for i in 2 1
-do
-    echo "$((i*5)) more seconds..."
-    sleep 5
-done
+while ! echo exit | nc localhost 8000; do sleep 5; done
+while ! echo exit | nc localhost 9000; do sleep 5; done
 python -m tools.manage_table create movie_reviews
 docker compose up internalproxy -d
 echo "Waiting for internal services to startup..."
-for i in 2 1
-do
-    echo "$((i*5)) more seconds..."
-    sleep 5
-done
+while ! echo exit | nc localhost 5002; do sleep 5; done
 echo "Setting up data..."
 python -m tools.upload_data tests/test_data.csv
 python -m tools.create_projection
 docker compose up -d
 echo "Waiting for website to start..."
-for i in 4 3 2 1
-do
-    echo "$((i*5)) more seconds..."
-    sleep 5
-done
+./scripts/wait_for_status.bash http://localhost:5000 200 300
 open http://localhost:5000
 docker-compose logs -f -t 
 
